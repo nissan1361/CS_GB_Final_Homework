ddc007815eab8aaa384b8653a4c2f926d18e32b9
Maxim Evlampiev <max1361@yandex.ru>
2023-05-03 21:47:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// My final CS GeekBrains Work
+
+Console.Write("Введите количество строк массива:");
+
+try
+{
+    int maxArray = int.Parse(Console.ReadLine());
+}
+catch(Exception)
+{
+    Console.WriteLine("Введенные данные не является целым числом.");
+}
+
+static string[] CreateArray(int maxLines = 20)
+{
+    string[] array = new string[maxLines];
+    return array;
+}
